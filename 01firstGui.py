22977403348a1ab0ed74b84b8148a11b552a544f
Aryantiwarib <143889805+Aryantiwarib@users.noreply.github.com>
2023-12-28 16:49:30 +0530

    Add files via upload

---
+++ 01firstGui.py
@@ -0,0 +1,8 @@
+# what is gui 
+#  GUI stands for Graphical User Interface. It is a type of user interface that allows users to interact with computers through graphical icons, visual indicators, and other graphical elements, rather than through text commands or other types of input. The full form of GUI is quite distinctive, and it is commonly found on most Windows, Android, and Mac computers.  
+
+from tkinter import *
+
+aryan_root=Tk()
+# gui source 
+aryan_root.mainloop()
